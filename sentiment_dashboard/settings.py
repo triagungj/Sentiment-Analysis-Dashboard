dfe6947a8593672dd5b984c3bdd60a366b5d6805
Tri Agung J <triagungj@gmail.com>
2025-08-13 22:44:54 +0700

    switch to postgresql

--- sentiment_dashboard/settings.py
+++ sentiment_dashboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'sentiment_dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'sentiment_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'sentiment_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'sentiment_pass'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': '5432',
     }
 }
 
