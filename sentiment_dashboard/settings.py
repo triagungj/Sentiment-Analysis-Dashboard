6eb2974c972500322dd6c0b56eb1cf69566526a3
Tri Agung J <triagungj@gmail.com>
2025-08-17 05:38:38 +0700

    add env example

--- sentiment_dashboard/settings.py
+++ sentiment_dashboard/settings.py
@@ -24,6 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
+SECURE_PROXY_SSL_HEADER = None if DEBUG else ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost, 127.0.0.1').split(', ')
 
 
